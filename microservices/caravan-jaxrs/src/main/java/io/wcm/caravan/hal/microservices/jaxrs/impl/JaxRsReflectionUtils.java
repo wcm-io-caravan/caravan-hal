3ac5da5e12675979cbbbca685ee68112c09a4bb4
Steffen Sauder <ssauder@pro-vision.de>
2019-01-07 11:36:04 +0100

    moved implementation code from JaxRsReflectionUtils to JaxRsLinkBuilderSupport

--- microservices/caravan-jaxrs/src/main/java/io/wcm/caravan/hal/microservices/jaxrs/impl/JaxRsReflectionUtils.java
+++ microservices/caravan-jaxrs/src/main/java/io/wcm/caravan/hal/microservices/jaxrs/impl/JaxRsReflectionUtils.java
@@ -19,239 +19,39 @@
  */
 package io.wcm.caravan.hal.microservices.jaxrs.impl;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Field;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.concurrent.ExecutionException;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 
 import org.apache.commons.lang3.reflect.FieldUtils;
-import org.apache.commons.lang3.tuple.Pair;
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.util.concurrent.UncheckedExecutionException;
+final class JaxRsReflectionUtils {
 
-public class JaxRsReflectionUtils {
-
-  private static final Cache<Class, PathParamFinder> PATH_PARAM_FINDER_CACHE = CacheBuilder.newBuilder().maximumSize(100).build();
-  private static final Cache<Class, QueryParamFinder> QUERY_PARAM_FINDER_CACHE = CacheBuilder.newBuilder().maximumSize(100).build();
-
-  public static Map<String, Object> getPathParameterMap(Object resourceImpl, Class clazz) {
-    try {
-      PathParamFinder finder = PATH_PARAM_FINDER_CACHE.get(clazz, () -> new PathParamFinder(clazz));
-      return finder.getParameterMap(resourceImpl);
-    }
-    catch (ExecutionException | UncheckedExecutionException ex) {
-      throw new RuntimeException("Failed to access @PathParam fields in " + clazz, ex);
-    }
+  private JaxRsReflectionUtils() {
+    // static methods only
   }
 
-
-  public static Map<String, Object> getQueryParameterMap(Object resourceImpl, Class clazz) {
-    try {
-      QueryParamFinder finder = QUERY_PARAM_FINDER_CACHE.get(clazz, () -> new QueryParamFinder(clazz));
-      return finder.getParameterMap(resourceImpl);
-    }
-    catch (ExecutionException | UncheckedExecutionException ex) {
-      throw new RuntimeException("Failed to access @QueryParam fields in " + clazz, ex);
-    }
-  }
-
-
-  private abstract static class AnnotatedParameterFinder<T extends Annotation> {
-
-    private final Class<T> annotationClass;
-
-    private final Collection<ParamValueProvider> namedValueProviders;
-
-    AnnotatedParameterFinder(Class clazz, Class<T> annotationClass) {
-
-      this.annotationClass = annotationClass;
-
-      this.namedValueProviders = Stream
-          .of(new AnnotatedFieldValueProviderFactory(), new AnnotatedConstructorValueProviderFactory(), new AnnotatedBeanParamValueProviderFactory())
-          .flatMap(f -> f.getValueProviders(this, clazz))
-          .collect(Collectors.toList());
-    }
-
-    Map<String, Object> getParameterMap(Object resourceImpl) {
-
-      Map<String, Object> parameterMap = new HashMap<>();
-
-      namedValueProviders.forEach(provider -> parameterMap.put(provider.getParamName(), provider.getParamValue(resourceImpl)));
-
-      return parameterMap;
-    }
-
-    private String getParameterNameFromAnnotation(AnnotatedElement p) {
-      T paramAnnotation = p.getAnnotation(annotationClass);
-      if (paramAnnotation == null) {
-        return null;
-      }
-      return getParameterNameFromAnnotation(paramAnnotation);
-    }
-
-    protected abstract AnnotatedParameterFinder<T> createFinderForBeanParamOfType(Class<?> type);
-
-    protected abstract String getParameterNameFromAnnotation(T paramAnnotation);
-
-  }
-
-  static class QueryParamFinder extends AnnotatedParameterFinder<QueryParam> {
-
-    QueryParamFinder(Class clazz) {
-      super(clazz, QueryParam.class);
-    }
-
-    @Override
-    protected AnnotatedParameterFinder<QueryParam> createFinderForBeanParamOfType(Class<?> type) {
-      return new QueryParamFinder(type);
-    }
-
-    @Override
-    protected String getParameterNameFromAnnotation(QueryParam paramAnnotation) {
-      return paramAnnotation.value();
-    }
-
-  }
-
-  static class PathParamFinder extends AnnotatedParameterFinder<PathParam> {
-
-    PathParamFinder(Class clazz) {
-      super(clazz, PathParam.class);
-    }
-
-    @Override
-    protected AnnotatedParameterFinder<PathParam> createFinderForBeanParamOfType(Class<?> type) {
-      return new PathParamFinder(type);
-    }
-
-    @Override
-    protected String getParameterNameFromAnnotation(PathParam paramAnnotation) {
-      return paramAnnotation.value();
-    }
-  }
-
-  private interface ParamValueProvider {
-
-    String getParamName();
-
-    Object getParamValue(Object resourceInstance);
-  }
-
-  private interface ParamValueProviderFactory {
-
-    Stream<ParamValueProvider> getValueProviders(AnnotatedParameterFinder<?> finder, Class clazz);
-  }
-
-  static class AnnotatedFieldValueProviderFactory implements ParamValueProviderFactory {
-
-    @Override
-    public Stream<ParamValueProvider> getValueProviders(AnnotatedParameterFinder<?> finder, Class clazz) {
-
-      return findFieldsDefinedInClass(clazz).stream()
-          .map(field -> Pair.of(finder.getParameterNameFromAnnotation(field), field))
-          .filter(pair -> pair.getKey() != null)
-          .map(pair -> new ParamValueProvider() {
-
-            @Override
-            public String getParamName() {
-              return pair.getKey();
-            }
-
-            @Override
-            public Object getParamValue(Object resourceInstance) {
-              return getFieldValue(pair.getValue(), resourceInstance);
-            }
-
-          });
-    }
-  }
-
-  static class AnnotatedConstructorValueProviderFactory implements ParamValueProviderFactory {
-
-    @Override
-    public Stream<ParamValueProvider> getValueProviders(AnnotatedParameterFinder<?> finder, Class clazz) {
-
-      return Stream.of(clazz.getConstructors())
-          .filter(constructor -> constructor.getParameterCount() > 0)
-          .flatMap(constructor -> Stream.of(constructor.getParameters()))
-          .map(constructorParam -> finder.getParameterNameFromAnnotation(constructorParam))
-          .filter(Objects::nonNull)
-          .distinct()
-          .map(name -> new ParamValueProvider() {
-
-            private Field field = getField(clazz, name);
-
-            @Override
-            public String getParamName() {
-              return name;
-            }
-
-            @Override
-            public Object getParamValue(Object resourceInstance) {
-              return getFieldValue(field, resourceInstance);
-            }
-
-          });
-    }
-  }
-
-  static class AnnotatedBeanParamValueProviderFactory implements ParamValueProviderFactory {
-
-    @Override
-    public Stream<ParamValueProvider> getValueProviders(AnnotatedParameterFinder<?> finder, Class clazz) {
-
-      return findFieldsThatContainOtherParamsIn(clazz)
-          .flatMap(beanField -> {
-
-            Collection<ParamValueProvider> beanProviders = finder.createFinderForBeanParamOfType(beanField.getType()).namedValueProviders;
-
-            return beanProviders.stream()
-                .map(provider -> new ParamValueProvider() {
-
-                  @Override
-                  public String getParamName() {
-                    return provider.getParamName();
-                  }
-
-                  @Override
-                  public Object getParamValue(Object resourceInstance) {
-                    Object beanValue = getFieldValue(beanField, resourceInstance);
-
-                    return getFieldValue(provider.getParamName(), beanValue);
-                  }
-
-                });
-          });
-    }
-  }
-
-  private static Stream<Field> findFieldsThatContainOtherParamsIn(Class clazz) {
+  static Stream<Field> findFieldsThatContainOtherParamsIn(Class clazz) {
 
     return findFieldsDefinedInClass(clazz).stream()
         .filter(field -> {
-          return FieldUtils.getAllFieldsList(field.getType()).stream()
+          return findFieldsDefinedInClass(field.getType()).stream()
               .anyMatch(nestedField -> nestedField.getAnnotation(QueryParam.class) != null || nestedField.getAnnotation(PathParam.class) != null);
         });
   }
 
-  private static List<Field> findFieldsDefinedInClass(Class clazz) {
-
+  static List<Field> findFieldsDefinedInClass(Class clazz) {
     return FieldUtils.getAllFieldsList(clazz);
   }
 
-  private static Object getFieldValue(String name, Object instance) {
+  static Field getField(Class clazz, String name) {
+    return FieldUtils.getField(clazz, name, true);
+  }
+
+  static Object getFieldValue(String name, Object instance) {
     if (instance == null) {
       return null;
     }
@@ -260,11 +60,7 @@ public class JaxRsReflectionUtils {
     return getFieldValue(field, instance);
   }
 
-  private static Field getField(Class clazz, String name) {
-    return FieldUtils.getField(clazz, name, true);
-  }
-
-  private static Object getFieldValue(Field field, Object instance) {
+  static Object getFieldValue(Field field, Object instance) {
     if (instance == null) {
       return null;
     }
